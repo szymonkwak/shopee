375283e1a047590e3abb0d9cd52cffbcd9658ad3
szymonkwak <szymon.kwak@gmail.com>
2022-08-28 22:52:49 +0200

    clean comment

--- src/store/cartSlice.ts
+++ src/store/cartSlice.ts
@@ -1,5 +1,4 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-// import type { PayloadAction } from '@reduxjs/toolkit';
 import { Product } from '../types/product';
 
 type CartState = Array<Product>;
